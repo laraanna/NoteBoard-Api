e776872fcdbce9a991d1a06eca06a56cf9b08208
laraanna <lara@laraannawagner.com>
2018-02-15 14:24:57 +0100

    Add create Note endpoint

--- app/controllers/api/v1/notes_controller.rb
+++ app/controllers/api/v1/notes_controller.rb
@@ -4,5 +4,15 @@ module Api::V1
       @notes = Note.all
       render json: @notes
     end
+
+    def create
+      @note = Note.create(note_params)
+      render json: @note
+    end
+
+    private
+    def note_params
+      params.require(:note).permit(:title, :body)
+    end
   end
 end
